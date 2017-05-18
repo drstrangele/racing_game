e02bef56d01dfb796bb292f534a65d1bbe909dc1
drstrangele <chickenle@gmail.com>
2017-05-18 09:04:04 -0700

    game changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
 
+  // start button - moves pie
+  $("#start").click(function(){
+    let pieWidth = $('#pie').width();
+    let trackWidth = $(window).width() - pieWidth;
+    $("#pie").animate({left: trackWidth}, "slow");
+
+    let pusheen = $("#pusheen");
+    let pusheen2 = $("#pusheen2");
+    if (pusheen.position().left + pusheen.width() >= trackWidth) {
+      $(document).off('keydown');
+      $("span").text(" Pusheen 1!");
+    } else if (pusheen.position().left + pusheen2.width() >= trackWidth) {
+      $(document).off('keydown');
+      $("span").text(" Pusheen 2! ");
+    };
+
+    // reset button
+    $("#reset").click(function(){
+      $("img").animate({left: "0"});
+    });
+
+    // character controls
+    $(document).keypress(function( event ) {
+      if ( event.key == "z" ) {
+        $("#pusheen").animate({left: "+=100px"},"slow");
+      } else if ( event.key == "m" ) {
+        $("#pusheen2").animate({left: "+=100px"},"slow");
+      }
+    });
+  });
 });
